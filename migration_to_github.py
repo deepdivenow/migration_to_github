742a75bb9a49522689ae66945b5e2c169af23573
Andrey Kiskyak <andrey@kislyak.com>
2018-04-12 21:08:17 +0300

    Add status check of import jobs

--- migration_to_github.py
+++ migration_to_github.py
@@ -8,7 +8,8 @@ config = configparser.ConfigParser(allow_no_value=True)
 config.read('config.ini')
 
 github_config={}
-github_config['auth']=(config['github']['username'],config['github']['password'])
+github_config['auth']=(config['github']['username'],
+                       config['github']['password'])
 github_config['headers']={
   "Accept": "application/vnd.github.barred-rock-preview",
   "user-agent": "my-app/0.0.1"
@@ -29,8 +30,8 @@ def migration_from_check(url,auth):
         return False
 
 def github_repo_check(url, auth):
-    if url[-1] == '/':
-        url=url[:-1]
+    # if url[-1] == '/':
+    #     url=url[:-1]
     r = requests.get(url,auth=auth)
     if 200 <= r.status_code < 300:
         return True
@@ -57,20 +58,21 @@ def github_import(paiload, github_config): #Paiload & github_config
         if r.status_code == 404:
             ra = requests.put(url_import,pl,headers=gc['headers'],auth=gc['auth'])
             if 200 <= ra.status_code < 300:
-                print ("Create new import GOOD for "+url_import)
+                print ("Create new import GOOD for: "+url_import)
             else:
-                print ("Create new import FAIL for "+url_import)
+                print ("Create new import FAIL for: "+url_import)
         elif r.status_code == 200:
             ra = requests.patch(url_import,pl,headers=gc['headers'],auth=gc['auth'])
             if 200 <= ra.status_code < 300:
-                print ("Patch import GOOD for "+url_import)
+                print ("Patch import GOOD for: "+url_import)
             else:
-                print ("Patch import FAIL for "+url_import)
+                print ("Patch import FAIL for: "+url_import)
         else:
-            print ("Unknown status code "+r.status_code+url_import)
+            print ("Unknown status code: "+r.status_code+" "+url_import)
     else:
         print("GirHub REPO not exists: "+url)
 
+error=0
 for migration_from in config['migration']:
     migration_to=config['migration'][migration_from]
     if migration_to is None:
@@ -80,5 +82,6 @@ for migration_from in config['migration']:
     github_import(paiload,github_config)
     if github_repo_check(get_url(github_config,"import"),github_config['auth']):
         print("Repo configuration status BAD: "+get_url(github_config,"import"))
+        error=1
 
-sys.exit(0)
+sys.exit(error)
